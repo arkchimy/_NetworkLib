938599adc60d7bfe259b73c335c2b1c6e3058f2d
DESKTOP-99KN4IA\정민혁 <arkchimy950323@gmail.com>
2026-05-13 01:12:43 +0900

    이해가 쉽도록  함수 순서 배치  재분배,
    checkAndHandleIoError 로 이름 변경 함수

--- IOCP_NetworkProject/NetworkLib.h
+++ IOCP_NetworkProject/NetworkLib.h
@@ -23,73 +23,73 @@
 
 namespace network
 {
-    enum eNetConfig
-    {
-        CONFIG_CONCURRENT_THREAD_CNT = 0,
-        CONFIG_WORKER_THREAD_CNT = 5,
-        CONFIG_RINGBUFFER_SIZE = 2048,
-        CONFIG_SESSION_MAX = 7000,
-        
-        CONFIG_SERVER_PORT = 32000,
-        CONFIG_SERVER_ADDR = 0,
+enum eNetConfig
+{
+    CONFIG_CONCURRENT_THREAD_CNT = 0,
+    CONFIG_WORKER_THREAD_CNT = 5,
+    CONFIG_RINGBUFFER_SIZE = 2048,
+    CONFIG_SESSION_MAX = 7000,
 
-        CONFIG_ZERO_COPY = 0,
-    };
-    struct WsadataRAII
+    CONFIG_SERVER_PORT = 32000,
+    CONFIG_SERVER_ADDR = 0,
+
+    CONFIG_ZERO_COPY = 0,
+};
+struct WsadataRAII
+{
+    WsadataRAII()
     {
-        WsadataRAII()
+        // WSAStartup 함수는 성공하면 0을 반환합니다.
+        if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
         {
-            // WSAStartup 함수는 성공하면 0을 반환합니다.
-            if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
-            {
-                RT_ASSERT(false);
-            }
+            RT_ASSERT(false);
         }
-        ~WsadataRAII()
-        {
-            WSACleanup();
-        }
-        WSADATA wsadata;
-    };
-    class NetworkLib
+    }
+    ~WsadataRAII()
     {
-      public:
-        NetworkLib();
-        virtual ~NetworkLib() = default;
+        WSACleanup();
+    }
+    WSADATA wsadata;
+};
+class NetworkLib
+{
+  public:
+    NetworkLib();
+    virtual ~NetworkLib() = default;
 
-      protected:
-        virtual void onAccept(const SeqAndIdx &sessionID) = 0;
-        virtual void onRecv() = 0;
-        virtual void onSend() = 0;
-        virtual void onRelease() = 0;
+  protected:
+    virtual void onAccept(const SeqAndIdx &sessionID) = 0;
+    virtual void onRecv() = 0;
+    virtual void onSend() = 0;
+    virtual void onRelease() = 0;
 
-      private:
-        void workerThread();
+  private:
+    void workerThread();
 
-        bool stackSessionIdx_Pop(ull &out);
-        void stackSessionIdx_Push(const ull &input);
+    void registerAcceptEx();
+    void completeAcceptEx(Session &session);
 
-        void errorChkRegister(Session &session, const int lastError);
-
-        void registerAcceptEx();
-        void registerRecv(Session &session);
-
-        void completeAcceptEx(Session& session);
-
-      private:
-        std::thread mWorkerThreads[CONFIG_WORKER_THREAD_CNT];
-        SOCKET mListenSock;
-
-        HANDLE mHcp; // iocpHandle
-        Session mSessions[CONFIG_SESSION_MAX];
-
-        std::stack<seqAddrType> mStackSessionIdx;
-        std::mutex mStackMutex;
-
-        seqAddrType mSeqID = -1;
-
-        WsadataRAII wsadata;
-    };
+    void registerRecv(Session &session);
 
 
-    } // namespace network
+    void checkAndHandleIoError(Session &session, const int lastError);
+
+    bool stackSessionIdx_Pop(ull &out);
+    void stackSessionIdx_Push(const ull &input);
+
+  private:
+    std::thread mWorkerThreads[CONFIG_WORKER_THREAD_CNT];
+    SOCKET mListenSock;
+
+    HANDLE mHcp; // iocpHandle
+    Session mSessions[CONFIG_SESSION_MAX];
+
+    std::stack<seqAddrType> mStackSessionIdx;
+    std::mutex mStackMutex;
+
+    seqAddrType mSeqID = -1;
+
+    WsadataRAII wsadata;
+};
+
+} // namespace network
